961dfe11a68f634b19f580d484642625b516e892
it24100597 <it24100597@my.sliit.lk>
2025-09-16 23:53:47 +0530

    Add lab 07

---
+++ Lab Sheet 07/IT24100597.R
@@ -0,0 +1,19 @@
+setwd("D:\\SLIIT\\Y2 S1\\PS\\LAB\\Lab 7")
+--(1)
+
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+--(2)
+pexp(2, rate=1/3)
+
+
+--(3)
+--i 
+1 - pnorm(130, mean=100, sd=15)
+
+
+--ii
+qnorm(0.95, mean=100, sd=15)
+
+
+
